8504ea42caa22bffb431e6d87300edc41fba414c
Carmela Pippa <carmelapippa@gmail.com>
2024-04-27 13:15:03 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hystorical parts documentation
---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>Hystorical Part Documentation</span>,
+  project: {
+    link: "https://github.com/shuding/nextra",
+  },
+  // ... other theme options
+};
