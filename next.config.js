6758838737ad5f7d620ecbf4a0a0022100745185
Carmela Pippa <carmelapippa@gmail.com>
2024-04-27 16:55:04 +0200

    Add system, images and others

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
+// const withNextra = require("nextra")({
+//   theme: "nextra-theme-docs",
+//   themeConfig: "./theme.config.jsx",
+// });
+
+// module.exports = withNextra();
+
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.jsx",
 });
 
-module.exports = withNextra();
+module.exports = withNextra({
+  images: {
+    domains: [], // Add domains here if your images are hosted externally
+    path: "/_next/image",
+    loader: "default", // This is typically 'default', adjust if you are using a different image loader
+  },
+});
