61454b90b78505f49981cff0c981fcdc467beb58
Carmela Pippa <carmelapippa@gmail.com>
2024-04-30 11:24:53 +0200

    add strapi logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5c4571ba2effd97"; }
+/******/ 		__webpack_require__.h = function() { return "c9f484997c2ba719"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
